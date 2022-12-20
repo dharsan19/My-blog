a43de41374891f9eef7f813d89ab1897937cc91b
Dharsan <idappadidharsan@gmail.com>
2022-12-20 08:37:48 +0530

    Feat: Navbar added

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>MY BLOG</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/create">NEW BLOG</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
