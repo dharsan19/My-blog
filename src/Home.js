0aac1c2110a8c7c0901936e642e04a27955313dc
Dharsan <idappadidharsan@gmail.com>
2022-12-21 12:39:32 +0530

    Update

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
+import Bloglist from "./Bloglist";
+
 const Home = () => {
+    const [blogs, setBlogs] = useState([
+        {title: 'My New blog', body: 'Lorem ipsum dolor sit amet consectetur.', author: 'lorem', id: 1},
+        {title: 'New Technology', body: 'Lorem ipsum dolor sit amet consectetur.', author: 'ipsum', id: 2},
+        {title: 'The Importance of IOT', body: 'Lorem ipsum dolor sit amet consectetur.', author: 'dolor', id: 3}
+    ])
+
     return ( 
         <div className="home">
-            <h2>HOMEPAGE</h2>
+            <Bloglist blogs= {blogs} title="All blogs"/>
         </div>
      );
 }
- 
+
 export default Home;
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,11 @@ const Navbar = () => {
             <h1>MY BLOG</h1>
             <div className="links">
                 <a href="/">Home</a>
-                <a href="/create">NEW BLOG</a>
+                <a href="/create" style={{ 
+                    color: 'white', 
+                    backgroundColor: '#f1356d',
+                    borderRadius: '8px' 
+                }}>New Blog</a>
             </div>
         </nav>
      );
